2bfddcfaa1124787295e4864110aed9e4d1461ae
Joseph Williams <josephwilliamsjs@gmail.com>
2021-04-08 14:00:39 -0400

    writing createUser function in userController

--- server/controllers/userController.ts
+++ server/controllers/userController.ts
@@ -1,9 +1,31 @@
-import mongoose from 'mongoose';
 import User from '../models/user';
 
-const createUser = () => {
-  console.log('user')
+// to do - replace any with type
+const createUser = (req: any, res: any) => {
+  const { name, email, password } = req.body;
+
+  User.findOne({ email })
+    .then(user => {
+      if (user) {
+        return res.status(400).json({error: 'User already exists'})
+      }
+
+      User.create({name, email, password}, (err, user) => {
+        if(err) {
+          console.log('ERROR: ', err)
+          return 
+        }
+        console.log('user created')
+        return res.status(201).json(user)
+      })
+    })
+    .catch(err => {
+      console.log('ERROR', err.message)
+    })
+  
 }
+
+
 const getUser = () => {
   console.log('user')
 }
--- server/dist/controllers/userController.js
+++ server/dist/controllers/userController.js
@@ -1,8 +1,30 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.updateUser = exports.getUser = exports.createUser = void 0;
-const createUser = () => {
-    console.log('user');
+const user_1 = __importDefault(require("../models/user"));
+// to do - replace any with type
+const createUser = (req, res) => {
+    const { name, email, password } = req.body;
+    user_1.default.findOne({ email })
+        .then(user => {
+        if (user) {
+            return res.status(400).json({ error: 'User already exists' });
+        }
+        user_1.default.create({ name, email, password }, (err, user) => {
+            if (err) {
+                console.log('ERROR: ', err);
+                return;
+            }
+            console.log('user created');
+            return res.status(201).json(user);
+        });
+    })
+        .catch(err => {
+        console.log('ERROR', err.message);
+    });
 };
 exports.createUser = createUser;
 const getUser = () => {
