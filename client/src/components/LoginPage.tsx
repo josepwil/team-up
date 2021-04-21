e14800f88e92ac830a01dd6461e69665b5fd70f0
Joseph Williams <josephwilliamsjs@gmail.com>
2021-04-21 19:44:51 -0400

    finshing ui for login page

--- client/src/components/LoginPage.tsx
+++ client/src/components/LoginPage.tsx
@@ -1,7 +1,17 @@
+import signIn from '../images/sign_in.jpeg'
+import '../styles/LoginPage.css'
+
 const LoginPage = () => {
   return(
-    <div>
-      I am the login page
+    <div className="loginPageContainer">
+      <div className="loginFormContainer">
+        <h3>Sign in</h3>
+        <input type="text" placeholder="email"/>
+        <input type="text" placeholder="password"/>
+        <button>sign in</button>
+      </div>
+      <img src={signIn} alt="aPlant" className="loginImage" />
+      
     </div>
   )
 }
