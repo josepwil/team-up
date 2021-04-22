d6c910413b796abba7cab675378739d63b403223
Joseph Williams <josephwilliamsjs@gmail.com>
2021-04-22 15:08:22 -0400

    creating project tile component

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,7 +1,12 @@
+import { useState } from 'react';
+import '../styles/Home.css'
+
 const Home = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
   return(
-    <div>
-      I am home
+    <div className="homeContainer">
+      <input className="searchInput" type="text" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} placeholder="...search for a project" />
     </div>
   )
 }
