c01eedacd76ce4e3e82026950fd13699186fcb04
Joseph Williams <josephwilliamsjs@gmail.com>
2021-04-23 10:23:54 -0400

    debouncing search input using lodash and useref

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,11 +1,23 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 import '../styles/Home.css'
 import ProjectList from './ProjectList'
+import debounce from 'lodash/debounce';
 
 const Home = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [projects, setProjects] = useState([]);
+
+const updateProject = (inputTerm: string) => {
+  console.log('updating ', inputTerm);
+}
+
+const updateProjectDebounced = useRef(debounce(updateProject, 150))
+
+const handleChange = (e: any) => {
+  setSearchTerm(e.target.value)
+  updateProjectDebounced.current(e.target.value);
+}
 
 useEffect(() => {
   axios.get('/api/projects')
@@ -19,7 +31,7 @@ useEffect(() => {
 
   return(
     <div className="homeContainer">
-      <input className="searchInput" type="text" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} placeholder="...search for a project" />
+      <input className="searchInput" type="text" value={searchTerm} onChange={handleChange} placeholder="...search for a project" />
       <ProjectList projects={projects}/>
     </div>
   )
