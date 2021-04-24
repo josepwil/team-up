1746bc3c157b792d86dd3335d1d162d7ec7f8527
Joseph Williams <josephwilliamsjs@gmail.com>
2021-04-24 13:43:18 -0400

    formatting technologies array in create project form

--- client/src/components/AddProjectForm.tsx
+++ client/src/components/AddProjectForm.tsx
@@ -1,7 +1,39 @@
+import { useState } from 'react';
+
 const AddProjectForm = () => {
+  const [name, setName] = useState('')
+  const [description, setDescription] = useState('')
+  const [technologies, setTechnologies] = useState('')
+  const [image, setImage] = useState<any>()
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault()
+    const technologiesArray = technologies.split(',').map(tech => tech.toLowerCase())
+    console.log(technologiesArray);
+  }
+
   return(
-    <div>
-      I am the add project form
+    <div className="loginFormContainer">
+      <h3>Create a new project</h3>
+      <form className="loginFormContainer" onSubmit={handleSubmit}>
+        <label htmlFor="name">
+          name
+          <input type="text" name="name" value={name} onChange={(e) => setName(e.target.value)} />
+        </label>
+        <label htmlFor="description">
+          description
+          <textarea name="description" value={description} onChange={(e) => setDescription(e.target.value)}/>
+        </label>
+        <label htmlFor="technologies">
+          technologies
+          <input type="text" name="technologies" placeholder="comma separated e.g. 'reactJS,PHP,mySQL" value={technologies} onChange={(e) => setTechnologies(e.target.value)}/>
+        </label>
+        <label htmlFor="image">
+          image
+          <input type="file" name="image" onChange={(e) => setImage(e.target.files![0])}/>
+        </label>
+        <input type="submit" value="create" />
+      </form>
     </div>
   )
 }
